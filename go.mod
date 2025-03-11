2609d0644108c728a52e0a0e94d91a6ea42246fb
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-03-11 12:49:19 +0530

    fixes and imports

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module "github.com/DivyanshuShekhar55/go-lexer"
+module github.com/DivyanshuShekhar55/go-parser
 
 go 1.23.3
+
+require github.com/DivyanshuShekhar55/go-lexer v0.0.0-20250311065949-76992c3c521c
--- main.go
+++ main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"fmt"
+
+	lexer "github.com/DivyanshuShekhar55/go-lexer"
+)
+
+func main(){
+	fmt.Println("hello")
+	source := "jjdj"
+	tokens:= lexer.Tokenise(source)
+	for _, item := range tokens{
+		fmt.Println(item)
+	}
+}
